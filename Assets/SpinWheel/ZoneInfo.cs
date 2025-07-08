a835e6a9bd133b0e87066bd56a6b5d42fe95bee3
Doğa Bircan Deviren <14931583+Thessin@users.noreply.github.com>
2025-07-08 03:53:36 +0300

    Added spin count to ZoneInfo.

--- Assets/SpinWheel/ZoneInfo.cs
+++ Assets/SpinWheel/ZoneInfo.cs
@@ -7,6 +7,13 @@ public class ZoneInfo
     public RewardInfo[] rewards;
     [HideInInspector]
     public ZoneType zoneType;
+    [Tooltip("Each spin count will rotate the wheel by 45 degrees. Randomly generates a value between given min/max values."), SerializeField]
+    private int spinCountMin, spinCountMax;
+
+    public int GetRandomSpinCount()
+    {
+        return UnityEngine.Random.Range(spinCountMin, spinCountMax);
+    }
 }
 
 public enum ZoneType
