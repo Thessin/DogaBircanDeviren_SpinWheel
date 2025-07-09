0901ffaa52472379243b8b6c3b9b3ff2c3cfa10d
Doğa Bircan Deviren <14931583+Thessin@users.noreply.github.com>
2025-07-09 16:48:39 +0300

    Added SpinWheelMenu script to orchestrate zones and spin wheel.

---
+++ Assets/SpinWheel/SpinWheelMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpinWheelMenu : MonoBehaviour
+{
+    [SerializeField]
+    private SpinWheelController spinWheelController;
+
+    [SerializeField]
+    private ZoneController zoneController;
+
+    [SerializeField]
+    private ZoneListWrapper zoneList;
+
+    private void OnEnable()
+    {
+        zoneController.OnZoneSelected += ZoneSelected;
+    }
+
+    private void OnDisable()
+    {
+        zoneController.OnZoneSelected -= ZoneSelected;
+    }
+
+    private void Awake()
+    {
+        spinWheelController.SetupController(zoneList.GetZoneInfo(0));
+        zoneController.SetupController(zoneList);
+    }
+
+    private void ZoneSelected(int zoneIndex)
+    {
+        spinWheelController.SetupController(zoneList.GetZoneInfo(zoneIndex));
+    }
+}
