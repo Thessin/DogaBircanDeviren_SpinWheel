d4d8b8c28a9ce793c9fc211d8e8eb53b4f34408b
Doğa Bircan Deviren <14931583+Thessin@users.noreply.github.com>
2025-07-09 17:04:31 +0300

    Fixed wrong parenting of zone buttons.

--- Assets/SpinWheel/Zones/MVC/ZoneView.cs
+++ Assets/SpinWheel/Zones/MVC/ZoneView.cs
@@ -8,13 +8,16 @@ public class ZoneView : MonoBehaviour
     [SerializeField]
     private ZoneButton btnGO;
 
+    [SerializeField]
+    private Transform zoneBtnParent;
+
     private List<ZoneButton> instantiatedBtns = new List<ZoneButton>();
 
     public void SetupZones(ZoneListWrapper zoneList, Action<int> OnBtnClicked)
     {
         for (int i = 0; i < zoneList.GetZoneListCount(); i++)
         {
-            ZoneButton btn = Instantiate(btnGO);
+            ZoneButton btn = Instantiate(btnGO, zoneBtnParent);
             btn.SetupBtn(i, zoneList.GetZoneType(i));
             btn.OnClicked += OnBtnClicked;
             instantiatedBtns.Add(btn);
