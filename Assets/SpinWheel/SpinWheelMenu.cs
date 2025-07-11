ea41223ce4bd78227d60e5b2f5eb7ef63fb907a6
Doğa Bircan Deviren <14931583+Thessin@users.noreply.github.com>
2025-07-11 18:05:35 +0300

    Fixed RetireBtn onclick not being recognized on apk builds.

--- Assets/SpinWheel/SpinWheelMenu.cs
+++ Assets/SpinWheel/SpinWheelMenu.cs
@@ -14,6 +14,7 @@ public class SpinWheelMenu : MonoBehaviour
     [SerializeField]
     private ZoneListWrapper zoneList;
 
+    [SerializeField, HideInInspector]
     private Button retireBtn;
 
     private void OnValidate()
